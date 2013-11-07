41d4c7f859ff3bbc45af56d6d0fa74613356bfa7
MrBadge <malcevanatoly@gmail.com>
2013-11-07 00:43:05 -0800

    Update ReallySmallProgramLoader.asm
    Should work now

--- ASMFiles/ReallySmallProgramLoader.asm
+++ ASMFiles/ReallySmallProgramLoader.asm
@@ -1,7 +1,7 @@
 ;com port must be initialized with parameters: 4800 bit/s, 8 bit, parity even, two stop bits
-
+;start address must be 2100h
 readByte:
-    IN 0xFB
+    IN 0xFB 
     ANI 0x02
     JZ readByte
     IN 0xFA
@@ -11,8 +11,8 @@ readByte:
     JNZ readByte
     RET
 
-MOV H, 0x?? ;the address must reference to the end of this loader, where main loader will start
-MOV L, 0x??
+MVI H, 21h ;the address must reference to the end of this loader, where main loader will start
+MVI L, 33h
 smallPrLoad:
     call readByte
     MOV A, B
