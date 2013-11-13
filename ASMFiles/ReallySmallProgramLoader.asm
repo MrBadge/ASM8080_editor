f0c3c83bb5a2edd4271978ff62fa2648921793dc
ABekirev <abekirevmac@gmail.com>
2013-11-13 20:04:58 +0400

    bug fixes

--- ASMFiles/ReallySmallProgramLoader.asm
+++ ASMFiles/ReallySmallProgramLoader.asm
@@ -1,13 +1,14 @@
 ;com port must be initialized with parameters: 4800 baud, 7 bit, parity even, two stop bits
 MVI H, 021h ;the address must reference to the end of this loader, where main loader will start
-MVI L, 023h
+MVI L, 02Fh
 init_timer_and_uart: 
     MVI A, 40h ; сброс
     OUT 0FBh
-    MVI A, FEh ; 
+    MVI A, 0FEh ; 
     OUT 0FBh
     MVI A, 05h ; (00 00 01 01) включение приема / передачи 
     OUT 0FBh
+
 JMP smallPrLoad
 readByte:
     IN 0FBh
